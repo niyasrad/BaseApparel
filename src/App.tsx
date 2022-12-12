493dbf3ced30ed3c2c6415ee251278056e410879
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2022-12-12 22:34:27 +0530

    responsive change

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,23 @@ import woman1 from './assets/images/hero-desktop.jpg';
 import woman2 from './assets/images/hero-mobile.jpg';
 import './App.css';
 
+
+function ValidateEmail(mail: string) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
 function App() {
   return (
     <div className="App">
       <div className="flex-one">
         <img className="bg" src={bg} alt="bg"/>
         <img src={logo} alt="logo" className='logo' />
+        <img src={woman2} alt='woman'className="woman-mobile"/>
         <div className="bottom">
           <h1><span className="brawn">WE'RE</span><br></br>COMING<br></br>SOON</h1>
           <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
